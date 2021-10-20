52040867901afaacf267b353818f711bd3407de2
Sean Nakamura <seanenakamura@gmail.com>
2021-10-19 18:45:32 -0700

    Update prefix for /opt/homebrew path

--- config/fish/conf.d/env.fish
+++ config/fish/conf.d/env.fish
@@ -5,6 +5,7 @@ set fish_greeting
 
 set g -x EDITOR nvim
 
+set -g -x N_PREFIX /opt/homebrew
 set -gx PATH /opt/homebrew/bin $PATH
 set -g -x GOPATH $HOME/code/go
 set -x PATH $PATH /usr/local/go/bin $GOPATH/bin
